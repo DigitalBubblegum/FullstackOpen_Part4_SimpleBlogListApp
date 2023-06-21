747cc5af1a7ecc6d1a9c7868ccf85d2a2c5562a8
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-06-21 11:13:24 +0530

    -refactoring code

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-require("dotenv").config()
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose.set('strictQuery',false)
 
-const mongoUrl = process.env.MONGODB_URI
-console.log("connecting to ", process.env.MONGODB_URI);
+logger.info("connecting to ", process.env.MONGODB_URI);
 mongoose
-  .connect(mongoUrl)
+  .connect(config.mongoUrl)
   .then(() => {
-    console.log("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+    logger.error("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 module.exports = app
-
-//end
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const config = require("./utils/config");
+const logger = require('./utils/logger')
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
