1cbc633eb5df2d7b61d8dfeb5b3522d10c2bb2b3
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-06-28 20:06:38 +0530

    -added a new helper function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,20 @@ const totalLikes = (blogs) => {
   return total
 };
 
-module.exports = {
-  dummy,totalLikes,
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  const maxBlog = blogs.reduce((maxi, blog) =>
+    blog.likes > maxi.likes ? blog : maxi
+  );
+
+  return maxBlog.likes;
 };
+
+module.exports = {
+  dummy,totalLikes,favoriteBlog,
+};
+
 
