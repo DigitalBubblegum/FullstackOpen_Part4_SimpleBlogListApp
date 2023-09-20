cc26ab0f79ece30361dbbb35362f2d3f7b1b4ff1
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-09-20 15:30:06 +0530

    -added test to check the response body length

--- tests/checkjson.test.js
+++ tests/checkjson.test.js
@@ -6,7 +6,12 @@ const api = supertest(app)
 test ('api returns json',
     async() =>{
         await api.get('/api/blogs').expect(200).expect('Content-Type',/application\/json/)
-})
+},100000)
+test('api returns 3 blogs',
+    async()=>{
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(3)
+},100000)
 afterAll(async()=>{
     await mongoose.connection.close();
 })
