b42674bdcfd941839ce242638342a6aa3832a277
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-09-20 15:17:15 +0530

    -added a test to check if API return json

---
+++ tests/checkjson.test.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+test ('api returns json',
+    async() =>{
+        await api.get('/api/blogs').expect(200).expect('Content-Type',/application\/json/)
+})
+afterAll(async()=>{
+    await mongoose.connection.close();
+})
