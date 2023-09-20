f8659cc17f1cccfc68a939abde4f0b55ab176a45
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-09-20 15:34:04 +0530

    -renamed file to api.test.js

--- tests/api.test.js
+++ tests/api.test.js
@@ -12,6 +12,11 @@ test('api returns 3 blogs',
         const response = await api.get('/api/blogs')
         expect(response.body).toHaveLength(3)
 },100000)
+test('api returns 0th blog as MongoDB world',
+    async()=>{
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].title).toBe('MongoDB world')
+},100000)
 afterAll(async()=>{
     await mongoose.connection.close();
 })
