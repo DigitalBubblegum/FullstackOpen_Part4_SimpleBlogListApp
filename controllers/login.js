2349d28624731431d007712af41459670957fc63
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-09-30 16:19:16 +0530

    -successful login also returns the user.id

--- controllers/login.js
+++ controllers/login.js
@@ -15,6 +15,6 @@ loginRouter.post('/',async(request,response) => {
 		id: user._id
 	}
 	const token = jwt.sign(userForToken,process.env.SECRET,{ expiresIn: 60*60 })
-	response.status(200).send({ token,username:user.username,name:user.name })
+	response.status(200).send({ token,username:user.username,name:user.name,id: user.id })
 })
 module.exports = loginRouter
